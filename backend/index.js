943309905550e058375334d2303a6b57998e7aec
Keshab60 <keshabg500@gmail.com>
2026-01-23 00:01:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -264,6 +264,10 @@ io.on("connection", (socket) => {
 
 
 // Start server
-server.listen(5000, () => {
-    console.log("Server is listening on port 5000");
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
+
